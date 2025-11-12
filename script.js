a2d84e3f9394fbd181152ba914ce4b6f927900e7
Gaurav22-01 <g2201p@gmail.com>
2025-11-12 09:35:05 +0530

    dropdown updated

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ function closeCoupon(){
     document.getElementById('coupon').style.visibility='hidden';
     document.getElementById('main').style.opacity='1';
 }
+
+function openMenuPage() {
+    var selectedPage = document.getElementById("menuSelect").value;
+    if (selectedPage) {
+        window.location.href = selectedPage;
+    }
+}
